25816ebe35e37b7d094b6de874a86e075b7ce128
Humayoun <humayoun.mani@gmail.com>
2023-07-09 19:51:28 +0500

    added flexwrapper commpn style

--- src/common/styles/CommonStyles.ts
+++ src/common/styles/CommonStyles.ts
@@ -1,11 +1,12 @@
 import { styled } from "styled-components"
 
-export const FlexWrapper = styled.div<{ flexDirection?: "row" | "column" }>`
+export const FlexWrapper = styled.div<{
+  flexDirection?: "row" | "column"
+  flexWrap?: "wrap" | "nowrap"
+}>`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: ${({ flexDirection }) => flexDirection || "row"};
-  flex-wrap: wrap;
-  border: 1px solid;
-  height: 100vh;
+  flex-wrap: ${({ flexWrap }) => flexWrap || "nowrap"};
 `
